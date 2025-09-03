de1fe5265e86cf5a344d004e86c610ccad4314d4
Bhoomika <bhoomika.jalandhra@gmail.com>
2025-09-03 14:11:12 +0530

    Add java program to show the use of some datatypes

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+class DataTypes{
+    public static void main(String[] args)
+    {
+        char c='A';
+        float f=3.14f;
+        double d=3.1232214532;
+        String s="Bhoomi";
+        System.out.println("Character: "+c);
+        System.out.println("Float: "+f);
+        System.out.println("Double: "+d);
+        System.out.println("String: "+s);
+    }
+}
