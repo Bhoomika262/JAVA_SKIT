689b06540c132400fd5b0ad5def15e48c0ca182f
Bhoomika <bhoomika.jalandhra@gmail.com>
2025-09-18 09:34:56 +0530

    Add java program to append string using stringbuilder

---
+++ StringAppend.java
@@ -0,0 +1,10 @@
+class StringAppend
+{
+    public static void main(String[] args)
+    {
+        StringBuilder sb= new StringBuilder("Java");
+        sb.append(" Programming");
+        sb.append(" Lab");
+        System.out.println("After append: "+sb);
+    }
+}
