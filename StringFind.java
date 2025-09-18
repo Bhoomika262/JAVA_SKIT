3b49b22204aa05162203af97ad26cea6ff7180f3
Bhoomika <bhoomika.jalandhra@gmail.com>
2025-09-18 09:03:56 +0530

    Add java program to extract and print a string from a given sentence

--- StringFind.java
+++ StringFind.java
@@ -1,10 +1,7 @@
-import java.util.Scanner;
-
 class StringFind
 {
     public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
         String sentence="This is a Java lab";
         String name=sentence.substring(10,sentence.length());
         System.out.print(name);
